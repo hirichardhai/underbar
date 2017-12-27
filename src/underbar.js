0a9eaa6d485bc892aea3e2ad24c4f9cf849b309b
Richard Hai <hirichardhai@gmail.com>
2017-12-26 17:31:17 -0800

    Completed _.filter tests in underbar.js

--- src/underbar.js
+++ src/underbar.js
@@ -83,6 +83,14 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var arr = [];
+
+    _.each(collection, function(item) {
+      if (test(item)) {
+        arr.push(item);
+      }
+    })
+    return arr;
   };
 
   // Return all elements of an array that don't pass a truth test.
